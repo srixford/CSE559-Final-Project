a1c5587a9d4dea25e14a0172212f91eb5ebc3a78
Sam <samrixford@gmail.com>
2025-12-11 03:43:43 -0700

    Atom mismatch issues - part 3

--- rmse_calculator.py
+++ rmse_calculator.py
@@ -15,8 +15,13 @@ for file in os.listdir(generated_directory):
         continue
 
     file_id = file.split("_")[0]
-    experiment_file = os.path.join(experiment_directory, file)
+    file_tcr = file.split("_")[4]
     generated_file = os.path.join(generated_directory, file)
+    experiment_file = ""
+
+    for file_exp in os.listdir(experiment_directory):
+        if file_tcr == file_exp.split("_")[4]:
+            experiment_file = os.path.join(experiment_directory, file_exp)
 
     if not os.path.exists(experiment_file):
         continue
@@ -27,19 +32,20 @@ for file in os.listdir(generated_directory):
     experiment_atoms = [atom.get_coord() for atom in experiment_structure.get_atoms()]
     generated_atoms = [atom.get_coord() for atom in generated_structure.get_atoms()]
 
-    if len(experiment_atoms) != len(generated_atoms):
-        rmse = "Atom mismatch"
-    else:
-        experiment_values = np.array(experiment_atoms)
-        generated_values = np.array(generated_atoms)
+    if experiment_file != "":
+        if len(experiment_atoms) != len(generated_atoms):
+            rmse = "Atom mismatch"
+        else:
+            experiment_values = np.array(experiment_atoms)
+            generated_values = np.array(generated_atoms)
 
-        rmse = np.sqrt(np.mean(((generated_values - experiment_values) ** 2).sum(axis=1)))
+            rmse = np.sqrt(np.mean(((generated_values - experiment_values) ** 2).sum(axis=1)))
 
 
-    results.append({
-        "id": file_id,
-        "rmse": rmse
-    })
+        results.append({
+            "id": file_id,
+            "rmse": rmse
+        })
 
 df = pd.DataFrame(results)
 df.to_csv("final_rmse.csv", index=False)
